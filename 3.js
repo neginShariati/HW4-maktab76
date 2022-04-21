eb0c645693f38dd22643cf63844d5b32ae306b74
nein <negin.s.sh@gmail.com>
2022-04-22 01:04:02 +0430

    3

---
+++ 3.js
@@ -0,0 +1,9 @@
+function upper(str){
+    let arr = str.split(' ');
+
+    for (let i =0 ; i< arr.length; i++){
+        arr[i]= arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join(' ');
+}
+console.log(upper('the quick brown fox'));
